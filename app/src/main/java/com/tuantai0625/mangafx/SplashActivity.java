16b45dbd8e1f0fd3dbaec607d40b5fd57b7179b5
Lê Tuấn Tài <tuantai0625@gmail.com>
2017-05-25 21:51:54 +0700

    Create Splash screen

---
+++ app/src/main/java/com/tuantai0625/mangafx/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.tuantai0625.mangafx;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new LoadingDataTask().execute();
+    }
+
+    private class LoadingDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            long endTime = System.currentTimeMillis() + 3*1000;
+            while (System.currentTimeMillis() < endTime) {
+                synchronized (this) {
+                    try {
+                        wait(endTime - System.currentTimeMillis());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
